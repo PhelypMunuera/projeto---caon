f35e867e824f06a249a5bd1483d65d44e3afbfc5
Phelyp <Phelyp>
2024-12-06 09:27:59 -0300

    js do login

---
+++ src/js/scriptIndex.js
@@ -0,0 +1,10 @@
+const loginButton = document.getElementsByClassName("buttonBase")[0];
+const checkRemeber = document.getElementById("checkRemeber");
+
+// // fazer um mideware para validar senha de usuario
+loginButton.onclick = function(){ 
+   window.location.href = "./src/html/selectAction.html"
+}
+
+
+
