e1a18fddae184af2c55617579f33fb4cb84e7e27
Phelyp <Phelyp>
2024-12-06 10:41:11 -0300

    pag consulta ok

---
+++ src/js/searchClient.js
@@ -0,0 +1,26 @@
+const actionButton = document.getElementById("confirmation")
+const homeButton = document.getElementById("homeButton")
+const valueCardSus = document.getElementById("cardSUSUser")
+
+// // fazer um mideware para validar senha de usuario
+actionButton.onclick = function(){ 
+   
+   const value = valueCardSus.value
+    
+        window.location.href = "../html/cadastrado.html"
+
+        console.log("if ")
+   /* if() { Aqui ficara a logica para entegra o banco de dados, para buscar usuarios 
+    }else {
+        alert("Úsuario Não Cadastrado")
+    }
+*/
+
+}
+
+
+
+homeButton.onclick = function(){
+   
+    window.location.href = "../html/selectAction.html"
+}
