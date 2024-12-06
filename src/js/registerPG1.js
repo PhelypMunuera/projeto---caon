a55b7714145e02cf22fbfd99c45d5452de7d2377
Phelyp <Phelyp>
2024-12-06 12:48:18 -0300

    family information okl

--- src/js/registerPG1.js
+++ src/js/registerPG1.js
@@ -2,13 +2,8 @@ const nextButton = document.getElementById("nextButton")
 const homeButton = document.getElementById("homeButton")
 
 nextButton.onclick = function(){ 
-
-        console.log("if ")
         window.location.href = "../html/registerClientFamily.html"
-
 }
-
-
 
 homeButton.onclick = function(){
     alert("Está ação perdera todo as informações. Deseja continuar?")
---
+++ src/js/registerPG2.js
@@ -0,0 +1,37 @@
+const nextButton = document.getElementById("nextButton")
+const homeButton = document.getElementById("homeButton")
+const moreMember = document.getElementById("moreMemberFamily")
+
+nextButton.onclick = function(){ 
+
+    window.location.href = "../html/registerMedicalInformation.html"
+       
+}
+
+
+
+homeButton.onclick = function(){
+    alert("Está ação perdera todo as informações. Deseja continuar?")
+    window.location.href = "../html/registerClient.html"
+
+}
+
+moreMember.onclick = function(){
+    const newSection = document.createElement("section")    
+    const container = document.getElementById("members");
+
+    newSection.innerHTML = `
+                <br>
+                <section id="containerId" class="newMenber">
+                    <input type="text" id="newMenberName" placeholder="Nome">
+                    <input type="text" id="newMenberOld" placeholder="idade">
+                    <input type="text" id="newMenberPararents" placeholder="Parentesco">
+                    <input type="text" id="newMenberEducation" placeholder="Escolaridade">
+                    <input type="text" id="newMenberProfession" placeholder="Profissão">
+                    <input type="text" id="newMenberIncome" placeholder="Renda">
+                </section>
+`
+
+container.appendChild(newSection); 
+
+}
